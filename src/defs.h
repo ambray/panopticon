dd477d31977a0e1755bf98b8239e33ea04dbf253
Aaron Bray <aaron.m.bray@gmail.com>
2018-10-24 15:53:19 -0500

    Adjusted macro, added logic to find security_hook_heads symbol

--- src/defs.h
+++ src/defs.h
@@ -4,7 +4,7 @@
 
 #if defined(DBG)
 #define dbg_print(x, ...) printk(KERN_ALERT "[panop]%s:%d> " #x\
-                                 "\n", __FILE__, __LINE__,##__VA_ARGS__)
+                                 "\n", __FILE__, __LINE__, ## __VA_ARGS__)
 #else
 #define dbg_print(x, ...) ""
 #endif
--- src/init.c
+++ src/init.c
@@ -1,17 +1,29 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/kallsyms.h>
+#include "defs.h"
 
 MODULE_LICENSE("GPL");
+
+#define HOOK_HEADS_NAME   "security_hook_heads"
+
+static void* hook_loc = NULL;
 
 
 static int __init panop_init(void)
 {
+  if(0 == (hook_loc = (void*)kallsyms_lookup_name(HOOK_HEADS_NAME))) {
+    dbg_print("Failed to find %s!", HOOK_HEADS_NAME);
+    return -1;
+  }
+
+  dbg_print("Hook location: 0x%lx", (unsigned long)hook_loc);
   return 0;
 }
 
 static void __exit panop_exit(void)
 {
-  
+  dbg_print("-> Exiting module");
 }
 
 module_init(panop_init);
