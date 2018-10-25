0717eba932571a09a78d668043c19c64352d3295
Aaron Bray <aaron.m.bray@gmail.com>
2018-10-25 16:42:29 -0500

    Added netlink support

--- src/init.c
+++ src/init.c
@@ -3,22 +3,29 @@
 #include <linux/kallsyms.h>
 #include <linux/lsm_hooks.h>
 #include <linux/rculist.h>
+#include <net/sock.h>
+#include <linux/netlink.h>
+#include <linux/skbuff.h>
 #include "defs.h"
 
 MODULE_LICENSE("GPL");
 
-#define HOOK_HEADS_NAME   "security_hook_heads"
-#define ADD_HOOKS_NAME    "security_add_hooks"
+#define HOOK_HEADS_NAME     "security_hook_heads"
+#define NETLINK_PANOPTICON  30
 
 /* Forward Declarations */
-static int panop_file_open(struct file* f, const struct cred* cred);
+static int  panop_file_open(struct file* f, const struct cred* cred);
+static void panop_nl_recv(struct sk_buff* skb);
 
 /* Globals */
+static struct sock* panop_sk = NULL;
 static void* hook_loc = NULL;
 static struct security_hook_list panop_hooks[] = {
   { .head = NULL, .hook = { .file_open = panop_file_open}, },
 };
-
+static struct netlink_kernel_cfg panop_nl_cfg = {
+  .input = panop_nl_recv,
+};
 
 static unsigned long unprotect(void)
 {
@@ -66,6 +73,21 @@ static void rem_hooks_from_list(struct security_hook_list* hooks, int count)
   reprotect(cr0);
 }
 
+
+static int init_netlink(struct sock** s)
+{
+  dbg_print("About to construct netlink socket...");
+
+  if(NULL == (*s = netlink_kernel_create(&init_net, NETLINK_PANOPTICON,
+                                         &panop_nl_cfg))) {
+    dbg_print("[x] Netlink socket creation failed!");
+    return -10;
+  }
+
+
+  return 0;
+}
+
 static void set_hooks(void)
 {
   struct security_hook_heads* hook_heads = NULL;
@@ -92,21 +114,31 @@ static void clear_hooks(void)
 
 static int __init panop_init(void)
 {
+  int res = 0;
+
   if(0 == (hook_loc = (void*)kallsyms_lookup_name(HOOK_HEADS_NAME))) {
     dbg_print("Failed to find %s!", HOOK_HEADS_NAME);
     return -1;
   }
 
+  if (0 != (res = init_netlink(&panop_sk))) {
+    dbg_print("Netlink creation failed");
+    goto done;
+  }
+
   dbg_print("Hook location: 0x%lx", (unsigned long)hook_loc);
   set_hooks();
   dbg_print("hooks set!");
-  return 0;
+done:
+  return res;
 }
 
 static void __exit panop_exit(void)
 {
   if(hook_loc)
     clear_hooks();
+  if(panop_sk)
+    netlink_kernel_release(panop_sk);
 
   dbg_print("-> Exiting module");
 }
@@ -114,9 +146,13 @@ static void __exit panop_exit(void)
 static int panop_file_open(struct file* f, const struct cred* cred)
 {
 
-  dbg_print("File Open event");
+  dbg_print("File Open: %s", f->f_path.dentry->d_iname);
 
   return 0;
+}
+
+static void panop_nl_recv(struct sk_buff* skb)
+{
 }
 
 module_init(panop_init);
